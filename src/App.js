ee97cb23a85cba81be1a0f4652169966a85ac73b
michel.vvs <michel.vvs@gmail.com>
2021-07-15 00:34:36 -0300

    voltando as aulas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [counter2, setCounter2] = useState(5);
+  //const [h1width, setH1width] = useState('10px');
+  //component did update = executa toda vez que o comp. atualiza
+
+    // com dependencia - executa em toda mudança da dependência
+    useEffect(() => {
+      setCounter2(counter2 + 1);
+    }, [counter]);
+
+
+  const increment = () => {
+    setCounter(counter + 1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Oi</h1>
+      <h3>C1: {counter}</h3>
+      <h3>C2: {counter2}</h3>
+      <button onClick={increment}>+</button>
     </div>
   );
 }
